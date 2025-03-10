b5391dad77bbc1a11af99eede2db20370555144a
Zhaharaaaaa <zhahara192@gmail.com>
2025-03-10 08:12:52 +0700

    tugas 3

---
+++ praktikum-3b/page/home.php
@@ -0,0 +1,184 @@
+
+<!-- Header-->
+<header class="py-5">
+    <div class="container px-5 pb-5">
+        <div class="row gx-5 align-items-center">
+            <!-- Kolom pertama untuk teks -->
+            <div class="col-12 col-md-7">
+                <div class="text-center text-md-start">
+                    <div class="badge text-white mb-4" style="background: linear-gradient(to right, #ff66b2, #ff99cc);">
+                        <div class="text-uppercase">Design &middot; Graphic &middot; Web</div>
+                    </div>
+                    <div class="fs-3 fw-light text-muted">I can help your creative design</div>
+                    <h1 class="display-3 fw-bolder mb-5">
+                        <span class="text-gradient d-inline" style="background: linear-gradient(to right, #ff66b2, #ff99cc); -webkit-background-clip: text; color: transparent;">
+                            Visual identity, instant impact
+                        </span>
+                    </h1>
+                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-md-start mb-3">
+                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="?page=resume" style="background: linear-gradient(to right, #ff66b2, #ff99cc);">Resume</a>
+                        <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="?page=projects">Projects</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Kolom kedua untuk gambar -->
+            <div class="col-12 col-md-5">
+                <div class="d-flex justify-content-center mt-5 mt-md-0">
+                    <div class="profile" style="background: linear-gradient(to right, #ff66b2, #ff99cc);">
+                        <img class="profile-img" src="assets/profil.png" alt="..." />
+                        <div class="dots-1">
+                        <!-- SVG Dots -->
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 191.6 1215.4" style="enable-background: new 0 0 191.6 1215.4" xml:space="preserve">
+                                    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)">
+                                        <path d="M227.7,12788.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,12801.6,289.7,12808.6,227.7,12788.6z"></path>
+                                        <path d="M1507.7,12788.6c-151-50-253-216-222-362c25-119,136-230,254-255c194-41,395,142,375,339c-11,105-90,213-190,262        C1663.7,12801.6,1569.7,12808.6,1507.7,12788.6z"></path>
+                                        <path d="M227.7,11508.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,11521.6,289.7,11528.6,227.7,11508.6z"></path>
+                                        <path d="M1507.7,11508.6c-151-50-253-216-222-362c25-119,136-230,254-255c194-41,395,142,375,339c-11,105-90,213-190,262        C1663.7,11521.6,1569.7,11528.6,1507.7,11508.6z"></path>
+                                        <path d="M227.7,10228.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,10241.6,289.7,10248.6,227.7,10228.6z"></path>
+                                        <path d="M1507.7,10228.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,10241.6,1569.7,10248.6,1507.7,10228.6z"></path>
+                                        <path d="M227.7,8948.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,8961.6,289.7,8968.6,227.7,8948.6z"></path>
+                                        <path d="M1507.7,8948.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,8961.6,1569.7,8968.6,1507.7,8948.6z"></path>
+                                        <path d="M227.7,7668.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,7681.6,289.7,7688.6,227.7,7668.6z"></path>
+                                        <path d="M1507.7,7668.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,7681.6,1569.7,7688.6,1507.7,7668.6z"></path>
+                                        <path d="M227.7,6388.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,6401.6,289.7,6408.6,227.7,6388.6z"></path>
+                                        <path d="M1507.7,6388.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,6401.6,1569.7,6408.6,1507.7,6388.6z"></path>
+                                        <path d="M227.7,5108.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,5121.6,289.7,5128.6,227.7,5108.6z"></path>
+                                        <path d="M1507.7,5108.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,5121.6,1569.7,5128.6,1507.7,5108.6z"></path>
+                                        <path d="M227.7,3828.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,3841.6,289.7,3848.6,227.7,3828.6z"></path>
+                                        <path d="M1507.7,3828.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,3841.6,1569.7,3848.6,1507.7,3828.6z"></path>
+                                        <path d="M227.7,2548.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,2561.6,289.7,2568.6,227.7,2548.6z"></path>
+                                        <path d="M1507.7,2548.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,2561.6,1569.7,2568.6,1507.7,2548.6z"></path>
+                                        <path d="M227.7,1268.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,1281.6,289.7,1288.6,227.7,1268.6z"></path>
+                                        <path d="M1507.7,1268.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,1281.6,1569.7,1288.6,1507.7,1268.6z"></path>
+                                    </g>
+                                </svg>
+                                <!-- END of SVG dots-->
+                            </div>
+        
+                    <div class="dots-3">
+                                <!-- SVG Dots-->
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 191.6 1215.4" style="enable-background: new 0 0 191.6 1215.4" xml:space="preserve">
+                                    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)">
+                                        <path d="M227.7,12788.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,12801.6,289.7,12808.6,227.7,12788.6z"></path>
+                                        <path d="M1507.7,12788.6c-151-50-253-216-222-362c25-119,136-230,254-255c194-41,395,142,375,339c-11,105-90,213-190,262        C1663.7,12801.6,1569.7,12808.6,1507.7,12788.6z"></path>
+                                        <path d="M227.7,11508.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,11521.6,289.7,11528.6,227.7,11508.6z"></path>
+                                        <path d="M1507.7,11508.6c-151-50-253-216-222-362c25-119,136-230,254-255c194-41,395,142,375,339c-11,105-90,213-190,262        C1663.7,11521.6,1569.7,11528.6,1507.7,11508.6z"></path>
+                                        <path d="M227.7,10228.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,10241.6,289.7,10248.6,227.7,10228.6z"></path>
+                                        <path d="M1507.7,10228.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,10241.6,1569.7,10248.6,1507.7,10228.6z"></path>
+                                        <path d="M227.7,8948.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,8961.6,289.7,8968.6,227.7,8948.6z"></path>
+                                        <path d="M1507.7,8948.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,8961.6,1569.7,8968.6,1507.7,8948.6z"></path>
+                                        <path d="M227.7,7668.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,7681.6,289.7,7688.6,227.7,7668.6z"></path>
+                                        <path d="M1507.7,7668.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,7681.6,1569.7,7688.6,1507.7,7668.6z"></path>
+                                        <path d="M227.7,6388.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,6401.6,289.7,6408.6,227.7,6388.6z"></path>
+                                        <path d="M1507.7,6388.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,6401.6,1569.7,6408.6,1507.7,6388.6z"></path>
+                                        <path d="M227.7,5108.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,5121.6,289.7,5128.6,227.7,5108.6z"></path>
+                                        <path d="M1507.7,5108.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,5121.6,1569.7,5128.6,1507.7,5108.6z"></path>
+                                        <path d="M227.7,3828.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,3841.6,289.7,3848.6,227.7,3828.6z"></path>
+                                        <path d="M1507.7,3828.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,3841.6,1569.7,3848.6,1507.7,3828.6z"></path>
+                                        <path d="M227.7,2548.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,2561.6,289.7,2568.6,227.7,2548.6z"></path>
+                                        <path d="M1507.7,2548.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,2561.6,1569.7,2568.6,1507.7,2548.6z"></path>
+                                        <path d="M227.7,1268.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C383.7,1281.6,289.7,1288.6,227.7,1268.6z"></path>
+                                        <path d="M1507.7,1268.6c-105-35-200-141-222-248c-43-206,163-412,369-369c155,32,275,190,260,339c-11,105-90,213-190,262        C1663.7,1281.6,1569.7,1288.6,1507.7,1268.6z"></path>
+                                    </g>
+                                </svg>
+                                <!-- END of SVG dots-->
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<!-- About Section-->
+<section class="bg-light py-5">
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-xxl-8">
+                <div class="text-center my-5">
+                    <h2 class="display-5 fw-bolder"><span class="display-5 fw-bolder" style="background: linear-gradient(to right, #ff66b2, #ff99cc); -webkit-background-clip: text; color: transparent;">About Me</span></h2>
+                    <p class="lead fw-light mb-4">Hallo saya Zhahara N Sukirman</p>
+                    <p class="text-muted">Saya mahasiswa aktif STT-NF prodi Sistem Informasi semester 2, Pengalaman selama 4 tahun dalam berbagai proyek desain telah mengasah kemampuan saya dalam menciptakan solusi inovatif dan berkualitas tinggi.</p>
+                    <div class="d-flex justify-content-center fs-2 gap-4">
+                        <a class="display-5 fw-bolder" style="background: linear-gradient(to right, #ff66b2, #ff99cc); -webkit-background-clip: text; color: transparent;" href="https://github.com/Zhaharaaaaa/web-2"><i class="bi bi-github"></i></a>
+                        <a class="display-5 fw-bolder" style="background: linear-gradient(to right, #ff66b2, #ff99cc); -webkit-background-clip: text; color: transparent;" href="https://www.linkedin.com/in/zhahara-n-sukirman-80a49633b/"><i class="bi bi-linkedin"></i></a>
+                        <a class="display-5 fw-bolder" style="background: linear-gradient(to right, #ff66b2, #ff99cc); -webkit-background-clip: text; color: transparent;"href="https://www.instagram.com/meezhhr/?hl=id"><i class="bi bi-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Projects Section-->
+<section class="py-5">
+    <div class="container px-5 mb-5">
+        <div class="text-center mb-5">
+            <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline" style="background: linear-gradient(to right, #ff66b2, #ff99cc); -webkit-background-clip: text; color: transparent;">Projects</span></h1>
+        </div>
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-11 col-xl-9 col-xxl-8">
+                <!-- Project Card 1-->
+                <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                <div class="card-body p-0">
+                    <div class="d-flex align-items-center">
+                        <div class="p-5 flex-grow-1" style="min-width: 200px;">
+                            <h2 class="fw-bolder">Project Poster</h2>
+                            <p>Project ini adalah pembuatan poster dengan tema crome, yang bertujuan untuk menyampaikan pesan estetik secara visual. Poster ini menggabungkan elemen desain minimalis, artistik, dan modern dengan perpaduan warna yang mencolok namun harmonis untuk menarik perhatian audiens.</p>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <img class="img-fluid" src="assets/profil1.jpg" alt="..." style="max-width: 400px; height: auto; object-fit: cover;"/>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+                <!-- Project Card 2-->
+                <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                <div class="card-body p-0">
+                    <div class="d-flex align-items-center">
+                        <div class="p-5 flex-grow-1" style="min-width: 200px;">
+                            <h2 class="fw-bolder">Project Illustration</h2>
+                            <p>Project ini adalah pembuatan ilusstrasi, yang bertujuan untuk menyampaikan pesan estetik secara visual. Ilusttrasi ini teknik semi realistis dengan perpaduan warna yang mencolok namun harmonis untuk menarik perhatian audiens.</p>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <img class="img-fluid" src="assets/model.jpg" alt="..." style="max-width: 400px; height: auto; object-fit: cover;"/>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+                <!-- Project Card 3-->
+                <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                <div class="card-body p-0">
+                    <div class="d-flex align-items-center">
+                        <div class="p-5 flex-grow-1" style="min-width: 200px;">
+                            <h2 class="fw-bolder">Project Web Design</h2>
+                            <p>Project Situs web ini dirancang dengan pendekatan modern, menggabungkan elemen-elemen visual yang menarik dan mudah digunakan. Dengan perpaduan warna yang harmonis, tata letak yang rapi, serta tipografi yang elegan, baik itu desktop, tablet, maupun smartphone. Teknologi seperti HTML, CSS, dan PHP digunakan untuk memastikan performa dan tampilan terbaik.</p>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <img class="img-fluid" src="assets/web.png" alt="..." style="max-width: 400px; height: auto; object-fit: cover;"/>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Call to action section-->
+<section class="py-5 text-white" >
+    <div class="container-fluid d-flex justify-content-center align-items-center" style="background: linear-gradient(to right, #ff66b2, #ff99cc); min-height: 300px; padding: 0;">
+    <div class="text-center">
+        <h2 class="display-4 fw-bolder mb-4">Let's build something together</h2>
+        <a class="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="?page=contact">Contact me</a>
+    </div>
+    </div>
+</section>
+
+
+</main>
+</body>
