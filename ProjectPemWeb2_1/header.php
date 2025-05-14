7f672b82a721d26ff6bba71b71bfda7e635ccb0c
Zhaharaaaaa <zhahara192@gmail.com>
2025-05-14 18:38:48 +0700

    Up Project01 UTS

---
+++ ProjectPemWeb2_1/header.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Kegiatan Dosen" />
+        <meta name="author" content=" zhahara " />
+        <title>Kegiatan Dosen dan Managemen Dosen</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link rel="icon" href="assets/img/KD.png" type="image/png" />
+        </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand-lg navbar-dark bg-dark " style="height: 75px;">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.php" style="font-size: 24px; ">
+                <i class="fas fa-graduation-cap" style="font-size: 30px;"></i> Kegiatan Dosen
+            </a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 m-3" id="sidebarToggle"><i class="fas fa-bars"></i></button>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4 d-flex justify-content-end">
+                <li class="nav-item m</li>s-auto "></li>
+                    <a class="nav-link d-flex align-items-center justify-content-end" id="navbar" href="index.php" role="button" aria-expanded="false"></a>
+                        <img src="assets/img/jara.jpg" alt="Profile" class="rounded-circle me-2" width="30" height="30" />
+                        <span class="d-none d-lg-inline text-light" style="font-size: 18px;">zhahara</span>
+                    </a>
+                </li>
+            </ul> 
+        </nav>
+    </body>
+</html>
